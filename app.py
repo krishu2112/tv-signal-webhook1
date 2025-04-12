3e7ada08b7e1d487aec28f376e572d75574cb91d
krishu2112 <Bhagyoday65akku@gmail.com>
2025-04-12 11:13:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def webhook():
     return jsonify({"status": "success", "message": "Alert received"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host=0.0.0.0',port=8000)
